df1e0ed7a2f3fe37808fd1c8ce6d24951899fae6
Ryan Pillsbury <ryanspillsbury90@gmail.com>
2015-04-21 17:38:06 -1000

    Added dynamic content to in progress view

--- Wayfarer-ios/Wayfarer-ios/WayInProgressViewController.swift
+++ Wayfarer-ios/Wayfarer-ios/WayInProgressViewController.swift
@@ -12,19 +12,49 @@ class WayInProgressViewController: UIViewController {
     
     var way: Way?
 
-    @IBOutlet weak var callToActionView: UIView!
+    @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var imageView: UIImageView!
 
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var subtitleLabel: UILabel!
+    
+    
+    @IBOutlet weak var tasksContainerView: UIView!
+    
+    @IBOutlet var taskLabels: [UILabel]!
+    
+    @IBOutlet weak var completedButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = way?.title
         self.way?.fetchImage(is2x: true, isAsync: true, callback: self.setImage)
+        setUIElements();
+    }
+    
+    private func setUIElements() {
+        self.titleLabel.text = self.way?.title.uppercaseString;
+        self.subtitleLabel.text = self.way?.subTitle;
+        self.completedButton.layer.borderWidth = 2;
+        self.completedButton.layer.borderColor = UIColor.whiteColor().CGColor;
+        var count = self.way?.tasks.count;
+        
+        while (count != taskLabels.count) {
+            taskLabels.removeLast().removeFromSuperview();
+        }
+        
+        for taskIndex in 0..<taskLabels.count {
+            var task = taskLabels[taskIndex];
+            task.text = self.way?.tasks[taskIndex]
+        }
     }
     
     private func setImage(id: Int) -> Void {
         if (self.way?.id == id) {
             self.imageView.image = self.way?.image2x;
         }
+        
     }
 
     override func didReceiveMemoryWarning() {
